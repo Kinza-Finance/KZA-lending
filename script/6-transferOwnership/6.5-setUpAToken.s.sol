f98fb47b1f5daf4619f7fd53aeb145e3713668cc
personal <128370392+MuesliW@users.noreply.github.com>
2023-06-14 10:40:36 +0800

    initialize new AToken impl during updating treasury

--- script/6-transferOwnership/6.5-setUpAToken.s.sol
+++ script/6-transferOwnership/6.5-setUpAToken.s.sol
@@ -41,6 +41,17 @@ contract setTreasury is Script {
             input.params = abi.encodePacked("0x10");
             configurator.updateAToken(input);
             }
+        // initialize the impl itself @TODO verify if this is needed
+        atoken.initialize(
+            pool,
+            address(0), // treasury
+            address(0), // underlyingAsset
+            IAaveIncentivesController(address(0)), // incentivesController
+            0, // aTokenDecimals
+            "ATOKEN_IMPL", // aTokenName
+            "ATOKEN_IMPL", // aTokenSymbol
+            "0x00" // param
+        );
         vm.stopBroadcast();
     }
 }
