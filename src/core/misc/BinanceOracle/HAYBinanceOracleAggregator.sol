b7f5628c94cc6ff212ddadc8f98820d79d2da6f3
VinciXie <szqq602@163.com>
2023-07-06 15:49:08 +0800

    binance oracle aggregator

--- src/core/misc/BinanceOracle/HAYBinanceOracleAggregator.sol
+++ src/core/misc/BinanceOracle/HAYBinanceOracleAggregator.sol
@@ -4,29 +4,51 @@ pragma solidity 0.8.10;
 import {AggregatorInterface} from "../../dependencies/chainlink/AggregatorInterface.sol";
 import {SID} from "../../dependencies/binance/SID.sol";
 import {IPublicResolver} from "../../dependencies/binance/IPublicResolver.sol";
+import {ITWAPAggregator} from "./ITWAPAggregator.sol";
+import {Ownable} from "../../dependencies/openzeppelin/contracts/Ownable.sol";
 
-contract BinanceOracleAggregator is AggregatorInterface {
+contract HAYBinanceOracleAggregator is Ownable, AggregatorInterface {
+    string public constant feedRegistrySID = "hay-usd.boracle.bnb";
+    bytes32 public constant nodeHash = 0xdefb391114b081d478abf3dc3f56caa145fee6ff97aedc4ff0342eb8b06da292;
     address public immutable sidRegistryAddress;
-    address public asset;
-    bytes32 nodeHash;
+    address twapAggregatorAddress;
 
-    constructor(address _sidRegistryAddress, address _asset, bytes32 _nodeHash) {
+    event SetTWAPAggregatorAddress(address twapAggregatorAddress);
+
+    constructor(address _sidRegistryAddress) {
         sidRegistryAddress = _sidRegistryAddress;
-        asset = _asset;
-        nodeHash = _nodeHash;
+    }
+
+    function setTWAPAggregatorAddress(address _twapAggregatorAddress) external onlyOwner {
+        twapAggregatorAddress = _twapAggregatorAddress;
+        emit SetTWAPAggregatorAddress(_twapAggregatorAddress);
     }
 
     function getFeedRegistryAddress() internal view returns (address) {
         SID sidRegistry = SID(sidRegistryAddress);
-        address publicResolverAddress = sidRegistry.resolver(nodeHash);
-        IPublicResolver publicResolver = IPublicResolver(publicResolverAddress);
+        address publicResolver = sidRegistry.resolver(nodeHash);
+        return IPublicResolver(publicResolver).addr(nodeHash);
+    }
 
-        return publicResolver.addr(nodeHash);
+    function getTWAP() private view returns (int256) {
+        return int256(ITWAPAggregator(twapAggregatorAddress).getTWAP());
+    }
+
+    function checkBinanceOracleAccess() external view returns (bool) {
+        AggregatorInterface feedRegistry = AggregatorInterface(getFeedRegistryAddress());
+        if (feedRegistry.latestAnswer() >= 0) {
+            return true;
+        }
+        return false;
     }
 
     function latestAnswer() external view returns (int256) {
         AggregatorInterface feedRegistry = AggregatorInterface(getFeedRegistryAddress());
-        return feedRegistry.latestAnswer();
+        try feedRegistry.latestAnswer() returns (int256 answer) {
+            return answer;
+        } catch {
+            return getTWAP();
+        }
     }
 
     function latestTimestamp() external view returns (uint256) {
---
+++ src/core/misc/BinanceOracle/ITWAPAggregator.sol
@@ -0,0 +1,4 @@
+
+interface ITWAPAggregator {
+  function getTWAP() external view returns (uint256);
+}
