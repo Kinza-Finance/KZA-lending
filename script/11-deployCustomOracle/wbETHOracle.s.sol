bb5cf9de2d1d3e996f9cd70d7690a0f99d40931a
personal <128370392+MuesliW@users.noreply.github.com>
2023-07-13 10:22:31 +0800

    update deploy script wbETH oracle to include eth address for subgraph dependency

--- script/11-deployCustomOracle/wbETHOracle.s.sol
+++ script/11-deployCustomOracle/wbETHOracle.s.sol
@@ -9,19 +9,22 @@ contract deployWbETHPriceAdaptor is Script {
         uint256 deployerPrivateKey;
         bool isProd = vm.envBool("isProd");
         address wbeth;
+        address weth;
         address ethAggregator;
         if (isProd) {
+            weth = vm.envAddress("WETH_PROD");
             wbeth = vm.envAddress("WBETH_PROD");
             ethAggregator = vm.envAddress("WETH_AGGREGATOR_PROD");
             deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         } else {
+            weth = vm.envAddress("WETH_TESTNET");
             wbeth = vm.envAddress("WBETH_TESTNET");
             ethAggregator = vm.envAddress("WETH_AGGREGATOR_TESTNET");
             deployerPrivateKey = vm.envUint("PRIVATE_KEY_TESTNET");
         }
         vm.startBroadcast(deployerPrivateKey);
 
-        new WbETHPriceAdaptor(ethAggregator, wbeth);
+        new WbETHPriceAdaptor(weth, ethAggregator, wbeth);
         
         vm.stopBroadcast();
     }
