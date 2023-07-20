d27d21a06e9f1dd76c49174e65304056c3dae0a0
personal <128370392+MuesliW@users.noreply.github.com>
2023-07-20 14:59:13 +0800

    add binanceOracle checker

---
+++ src/core/misc/BinanceOracle/WBETHBinanceOracleAggregator.sol
@@ -0,0 +1,91 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.10;
+
+import {AggregatorInterface} from "../../dependencies/chainlink/AggregatorInterface.sol";
+import {SID} from "../../dependencies/binance/SID.sol";
+import {IPublicResolver} from "../../dependencies/binance/IPublicResolver.sol";
+import {Ownable} from "../../dependencies/openzeppelin/contracts/Ownable.sol";
+
+interface IChainlinkAggregatorProxy {
+    function latestAnswer() external view returns (int256);
+}
+
+contract WBETHBinanceOracleAggregator is Ownable, AggregatorInterface {
+    string private constant feedRegistrySID = "wbeth-usd.boracle.bnb";
+    // @TODO add the nodeHash for wbeth
+    bytes32 private constant nodeHash = 0xf4132c82e8606c3c23c6e0e05f3fdaa88d1d56188433bc36a95c2f89504f31d2;
+    address public immutable sidRegistryAddress;
+    address public chainlinkAggregatorProxy;
+
+    event SetChainlinkAggregatorProxyAddress(address NewChainlinkAggregatorProxy);
+
+    constructor(address _sidRegistryAddress, address _chainlinkAggregator) {
+        sidRegistryAddress = _sidRegistryAddress;
+        chainlinkAggregatorProxy = _chainlinkAggregator;
+    }
+
+    function setChainlinkAggregatorAddress(address _chainlinkAggregatorProxy) external onlyOwner {
+        chainlinkAggregatorProxy = _chainlinkAggregatorProxy;
+        emit SetChainlinkAggregatorProxyAddress(_chainlinkAggregatorProxy);
+    }
+
+    function getFeedRegistryAddress() public view returns (address) {
+        SID sidRegistry = SID(sidRegistryAddress);
+        address publicResolver = sidRegistry.resolver(nodeHash);
+        return IPublicResolver(publicResolver).addr(nodeHash);
+    }
+
+    function checkBinanceOracleAccess() external view returns (bool) {
+        AggregatorInterface feedRegistry = AggregatorInterface(getFeedRegistryAddress());
+        try feedRegistry.latestAnswer() returns (int256) {
+            return true;
+        } catch {
+            return false;
+        }
+    }
+
+    /**
+     * @dev Converts an unsigned uint256 into a signed int256.
+     *
+     * Requirements:
+     *
+     * - input must be less than or equal to maxInt256.
+     */
+
+    function getChainlinkAnswer() internal view returns (int256) {
+        return IChainlinkAggregatorProxy(chainlinkAggregatorProxy).latestAnswer();
+    }
+
+    function latestAnswer() external view returns (int256) {
+        AggregatorInterface feedRegistry = AggregatorInterface(getFeedRegistryAddress());
+        try feedRegistry.latestAnswer() returns (int256 answer) {
+            return answer;
+        } catch {
+            return getChainlinkAnswer();
+        }
+    }
+
+    function latestTimestamp() external view returns (uint256) {
+        AggregatorInterface feedRegistry = AggregatorInterface(getFeedRegistryAddress());
+        return feedRegistry.latestTimestamp();
+    }
+
+    function latestRound() external view returns (uint256) {
+        AggregatorInterface feedRegistry = AggregatorInterface(getFeedRegistryAddress());
+        return feedRegistry.latestRound();
+    }
+
+    function getAnswer(uint256 roundId) external view returns (int256) {
+        AggregatorInterface feedRegistry = AggregatorInterface(getFeedRegistryAddress());
+        return feedRegistry.getAnswer(roundId);
+    }
+
+    function getTimestamp(uint256 roundId) external view returns (uint256) {
+        AggregatorInterface feedRegistry = AggregatorInterface(getFeedRegistryAddress());
+        return feedRegistry.getTimestamp(roundId);
+    }
+
+    function tryCustomOracleAnswer(address _customOracle) public view returns(int256) {
+        return AggregatorInterface(_customOracle).latestAnswer();
+    }
+}
