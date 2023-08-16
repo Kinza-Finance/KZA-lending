421ce64b93781e11942d57080d93b4b1de8ad644
personal <128370392+MuesliW@users.noreply.github.com>
2023-08-17 07:58:21 +0800

    reduce gas cost on decimals() and fail on constructor if call for decimals() fail

--- src/periphery/pToken/ProtectedERC20.sol
+++ src/periphery/pToken/ProtectedERC20.sol
@@ -7,18 +7,21 @@ import "@openzeppelin/token/ERC20/extensions/IERC20Metadata.sol";
 
 contract ProtectedERC20 is ERC20, ERC20Wrapper {
 
+    uint8 internal immutable decimal;
     constructor(address underlying, string memory underlyingName, string memory underlyingSymbol) 
     ERC20(
         string(abi.encodePacked("Kinza Protected ", IERC20Metadata(underlying).name())),
         string(abi.encodePacked("p", IERC20Metadata(underlying).symbol()))
         )
     ERC20Wrapper(IERC20(underlying)) {
+        decimal = IERC20Metadata(underlying).decimals();
     }
 
     function decimals() public view virtual override(ERC20, ERC20Wrapper) returns (uint8) {
+        // ERC20 would return 18
         // ERC20Wrapper would use try catch to return decimal 18 if the external read fails
-        // this would rather fail and revert if the underlying decimal read does not return
-        return IERC20Metadata(address(underlying())).decimals();
+        // technically we want to support underlying with any number of decimals
+        return decimal;
     }
 
 }
