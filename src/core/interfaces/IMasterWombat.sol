ce4e6ebacaab8437107ec71245acd8a44e5b6119
personal <128370392+MuesliW@users.noreply.github.com>
2023-09-11 21:52:52 +0800

    refactor levHelper to work for generalized underlying and lp

--- src/core/interfaces/IMasterWombat.sol
+++ src/core/interfaces/IMasterWombat.sol
@@ -5,7 +5,8 @@ interface IMasterWombat {
     function multiClaim(uint256[] calldata pids) external;
     function deposit(uint256 pid, uint256 amount) external;
     function withdraw(uint256 pid, uint256 amount) external;
-    function getAssetPid(address asset) external returns(uint256);
+    function getAssetPid(address asset) external view returns (uint256);
+    function poolInfo(uint256 pid) external view returns (address, address, uint40, uint128, uint128, uint104, uint104, uint40);
     function pendingTokens(
         uint256 _pid,
         address _user
--- src/periphery/misc/WombatLeverageHelper.sol
+++ src/periphery/misc/WombatLeverageHelper.sol
@@ -1,5 +1,6 @@
 import '../../core/interfaces/IPoolAddressesProvider.sol';
 import '../../core/interfaces/IPool.sol';
+import '../../core/interfaces/IAaveOracle.sol';
 import '../../core/interfaces/IMasterWombat.sol';
 import {IERC20} from '@aave/core-v3/contracts/dependencies/openzeppelin/contracts/IERC20.sol';
 
@@ -9,73 +10,79 @@ interface IBorrowableProvider {
     function getUserMaxBorrowable(address user, address asset) external view returns(uint256);
 }
 
+// just need one function for wombat LP Asset
+interface IAsset  {
+    function underlyingToken() external view returns(address);
+
+}
+
 contract WombatLeverageHelper {
     IPoolAddressesProvider immutable public provider;
-    IERC20 public underlying;
-    IERC20 public lp;
     IMasterWombat public masterWombat;
     IPool public pool;
-    uint8 emodeCategory;
-    uint256 pid;
-    constructor(address _provider, address _masterWombat, address _underlying, uint8 _emodeCategory) {
-        emodeCategory = _emodeCategory;
-        provider = IPoolAddressesProvider(_proivder);
-        underlying = IERC20(_underlying);
+    constructor(address _provider, address _masterWombat) {
+        provider = IPoolAddressesProvider(_provider);
         pool = IPool(IPoolAddressesProvider(_provider).getPool());
         masterWombat = IMasterWombat(_masterWombat);
-        underlying.approve(_masterWombat, type(uint256).max);
-        //approve LP for pool
-        uint256 _pid = masterWombat.getAssetPid(underlying);
-        (address lpAddr,,,,,,) = masterWombat.poolInfo(_pid);
-        pid = _pid;
-        lp = IERC20(lpAddr);
-        lp.approve(address(pool), type(uint256).max);
     }
 
-
-    function calculateUnderlyingBorrow(uint256 targeHF, uint256 deposit, uint8 emode) view returns(uint256) {
-        (uint256 underlyingPrice, uint256 lpPrice) = getLpAndUnderlyingPrice();
+    function calculateUnderlyingBorrow(uint256 targetHF, address lpAddr, uint256 depositAmount, uint8 emodeCategory) public view returns(uint256) {
+        address underlying = IAsset(lpAddr).underlyingToken();
+        (uint256 underlyingPrice, uint256 lpPrice) = getPrice(underlying, lpAddr);
         // assume a person without any other position
-        uint256 emodeLiqT = uint256(pool.getEModeCategoryData(emode).liquidationThreshold);
+        uint256 emodeLiqT = uint256(pool.getEModeCategoryData(emodeCategory).liquidationThreshold);
         // targetHF = depositTotal * emodeLiqT / borrowedUnderlyingTotal
         // targetHF = (deposit + borrow) * emodeLiqT * lpPrice / borrow * underlyingPrice
         // targetHF = (deposit * LiqT * lpPrice) / borrow * underlyingPrice + LiqT * emodeLiqT / underlyingPrice
         // after refactor: the constant part => depositRatio.
-        uint256 depositRatio = targetHF - 1e18 * lPrice * emodeLiqT / underlyingPrice / 10000;
-        return 1e18 * deposit * lpPrice * emodeLiqT / (depositRatio * underlyingPrice) / 10000;
+        uint256 depositRatio = targetHF - 1e18 * lpPrice * emodeLiqT / underlyingPrice / 10000;
+        return 1e18 * depositAmount * lpPrice * emodeLiqT / (depositRatio * underlyingPrice) / 10000;
 
     }
-    // this function just leverage the calculation function, 
-    // and loop the needed borrowedAmount
-    function loop(address borrowableProvider, uint256 targetHF, uint256 amount) external returns(uint256) {
-        require(targeHF > 1e18 && amount > 0, "targetHF or deposit amount invalid");
-        (uint256 underlyingPrice, uint256 lpPrice) = getLpAndUnderlyingPrice();
-        uint256 borrowTotal = calculateUnderlyingBorrow(targetHF, amount, emodeCategory);
+    // this function just leverage by borrowing underlying for the user
+    // and loop the needed borrowedAmount by re-depositing back the lp
+    // the final health factor may vary from the targetHF
+    // since the deposited LP might be different depends on wombat
+    function loop(address borrowableProvider, uint256 targetHF, address lpAddr, uint256 amount, uint8 emodeCategory) external returns(uint256) {
+        require(targetHF > 1e18 && amount > 0, "targetHF or deposit amount invalid");
+        address underlying = IAsset(lpAddr).underlyingToken();
+        uint256 pid = masterWombat.getAssetPid(lpAddr);
+        (uint256 underlyingPrice, uint256 lpPrice) = getPrice(underlying, lpAddr);
+        uint256 borrowTotal = calculateUnderlyingBorrow(targetHF, lpAddr, amount,  emodeCategory);
+        // all borrow would be deposited to mastwombat first
+        if (IERC20(underlying).allowance(address(masterWombat), address(this)) < borrowTotal) {
+            IERC20(underlying).approve(address(masterWombat), type(uint256).max);
+        }
+        // all borrow would be deposited to pool at the end
+        if (IERC20(lpAddr).allowance(address(pool), address(this)) < borrowTotal) {
+            IERC20(lpAddr).approve(address(pool), type(uint256).max);
+        }
         uint256 borrowed;
         while(borrowed < borrowTotal) {
             // start borrow
             uint256 diff = borrowTotal - borrowed;
-            uint256 toBorrow = IBorrowableProvider(borrowableProvider).getUserMaxBorrowable(msg.sender, underlying);
+            uint256 toBorrow = IBorrowableProvider(borrowableProvider).getUserMaxBorrowable(msg.sender, address(underlying));
             if (toBorrow == 0) {
                 // somehow the user can not borrow anymore
+                // can be due to other existing borrow, or debt ceiling/available in the protocol
                 break;
             }
             if (toBorrow > diff) {
                 toBorrow = diff;
             }
-            pool.borrow(underlying, toBorrow, 0, msg.sender);
             borrowed += toBorrow;
+            // 2 = variable interest rate, 0 = referral code
+            pool.borrow(underlying, toBorrow, 2, 0, msg.sender);
             // convert to LP on wombat
             masterWombat.deposit(pid, toBorrow);
             // deposit the LP into pool
-            uint256 amount = lp.balanceOf(address(this));
-            pool.deposit(lp, amount, msg.sender, 0);
+            uint256 amount = IERC20(lpAddr).balanceOf(address(this));
+            pool.deposit(lpAddr, amount, msg.sender, 0);
         }
         return borrowed;
     }
 
-    function getLpAndUnderlyingPrice() public view returns(uint256, uint256) {
-        IPoolDataProvider dataProvider = IPoolDataProvider(provider.getPoolDataProvider());
+    function getPrice(address underlying, address lp) public view returns(uint256, uint256) {
         IAaveOracle oracle = IAaveOracle(provider.getPriceOracle());
         address[] memory assets = new address[](2);
         assets[0] = underlying;
