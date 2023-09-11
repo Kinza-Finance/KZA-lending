6782f1f441c17cd60f8fac9754603e17c060c36f
personal <128370392+MuesliW@users.noreply.github.com>
2023-09-11 23:14:57 +0800

    add loop test to levHelper

--- test/ATokenStaker/WombatStaker/leverage.t.sol
+++ test/ATokenStaker/WombatStaker/leverage.t.sol
@@ -1,10 +1,11 @@
 
 import {ATokenWombatStakerBaseTest} from "./ATokenWombatStakerBaseTest.t.sol";
 import {IERC20} from "../../../src/core/dependencies/openzeppelin/contracts/IERC20.sol";
+import {IPoolDataProvider} from "../../../src/core/interfaces/IPoolDataProvider.sol";
 import {WombatLeverageHelper} from "../../../src/periphery/misc/WombatLeverageHelper.sol";
 import {ADDRESSES_PROVIDER, POOLDATA_PROVIDER, ACL_MANAGER, POOL, POOL_CONFIGURATOR, EMISSION_MANAGER, 
         ATOKENIMPL, SDTOKENIMPL, VDTOKENIMPL, TREASURY, POOL_ADMIN, HAY_AGGREGATOR, HAY, 
-        MASTER_WOMBAT, SMART_HAY_LP, LIQUIDATION_ADAPTOR} from "test/utils/Addresses.sol";
+        MASTER_WOMBAT, SMART_HAY_LP, LIQUIDATION_ADAPTOR, BORROWABLE_DATA_PROVIDER} from "test/utils/Addresses.sol";
 
 contract leverageTest is ATokenWombatStakerBaseTest {
     WombatLeverageHelper internal levHelper;
@@ -17,5 +18,23 @@ contract leverageTest is ATokenWombatStakerBaseTest {
 
     function test_leverageSetUp() public {
 
+    }
+
+    function test_loop() public {
+        uint256 targetHF = 1.2 * 1e18;
+        uint256 depositAmount = 100 * 1e18;
+        address bob = address(1);
+        deposit(bob, depositAmount, underlying);
+        turnOnEmode(bob);
+        loop(bob, targetHF, depositAmount);
+    }
+
+    function loop(address user, uint256 targetHF, uint256 depositAmount) public {
+        vm.startPrank(user);
+        // check variableDebt of the underlying asset is indeed the borrowed.
+        (,,address vDebtProxy) = IPoolDataProvider(POOLDATA_PROVIDER).getReserveTokensAddresses(HAY);
+        uint256 debtBefore = IERC20(vDebtProxy).balanceOf(user);
+        uint256 borrowed = levHelper.loop(BORROWABLE_DATA_PROVIDER, targetHF, underlying, depositAmount, eModeCategoryId);
+        assertEq(debtBefore + borrowed, IERC20(vDebtProxy).balanceOf(user));
     }
 }
