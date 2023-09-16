ed1fd8c03229a8af393c88adaf731102cc6c470c
personal <128370392+MuesliW@users.noreply.github.com>
2023-09-16 09:48:00 +0800

    remove unneeded interface

--- src/core/interfaces/IEmissionAggregator.sol
+++
@@ -1,4 +0,0 @@
-// the primary purpose is to consolidate reward, since each reward can only be set by 1 address on emissionManager
-interface IEmissionAggregator {
-    function notify(address token, uint256 amount) external;
-}
