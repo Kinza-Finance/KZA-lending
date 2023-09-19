b8233dfc5de54deccd97eb55bd53c9cd829df7c4
personal <128370392+MuesliW@users.noreply.github.com>
2023-09-19 15:32:08 +0800

    Revert "Update ValidationLogic.sol"
    This reverts commit 5b86f64fc3ae4cef7a908866845a3af1c63d649c.

--- contracts/protocol/libraries/logic/ValidationLogic.sol
+++ contracts/protocol/libraries/logic/ValidationLogic.sol
@@ -239,6 +239,7 @@ library ValidationLogic {
       })
     );
 
+    require(vars.userCollateralInBaseCurrency != 0, Errors.COLLATERAL_BALANCE_IS_ZERO);
     require(vars.currentLtv != 0, Errors.LTV_VALIDATION_FAILED);
 
     require(
