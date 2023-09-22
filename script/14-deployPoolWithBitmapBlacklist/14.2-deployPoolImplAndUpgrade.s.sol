cf976762efc2666740256379048255d455835140
personal <128370392+MuesliW@users.noreply.github.com>
2023-09-22 15:16:19 +0800

    add deploy and upgrade script

---
+++ script/14-deployPoolWithBitmapBlacklist/14.2-deployPoolImplAndUpgrade.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity 0.8.10;
+
+import "forge-std/Script.sol";
+import "../../src/core/interfaces/IPoolAddressesProvider.sol";
+import "../../src/core/protocol/pool/Pool.sol";
+contract deployPoolImpl is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        address provider = vm.envAddress("PoolAddressesProvider");
+        vm.startBroadcast(deployerPrivateKey);
+        Pool pool = new Pool(IPoolAddressesProvider(provider));
+        pool.initialize(IPoolAddressesProvider(provider));
+        IPoolAddressesProvider(provider).setPoolImpl(address(pool));
+        vm.stopBroadcast();
+
+    }
+}
