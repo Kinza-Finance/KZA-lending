04692a17289ed0bdd9acdd59a8409750084adb8c
personal <128370392+MuesliW@users.noreply.github.com>
2023-11-21 12:44:35 +0300

    update deploy script

--- script/7-deployRead/7.3.5-liquidationAdaptorWithAC.s.sol
+++ script/7-deployRead/7.3.5-liquidationAdaptorWithAC.s.sol
@@ -8,9 +8,11 @@ contract deployLiquidationDataProvider is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         address provider = vm.envAddress("PoolAddressesProvider");
+        address v3Fallback = vm.envAddress("PancakeV3Fallback");
         vm.startBroadcast(deployerPrivateKey);
 
-        new AaveV2CrossTokenLiqAdatorAccessControl(provider);
+        AaveV2CrossTokenLiqAdatorAccessControl liq = new AaveV2CrossTokenLiqAdatorAccessControl(provider);
+        liq.updateV3Fallback(v3Fallback);
         
         vm.stopBroadcast();
     }
